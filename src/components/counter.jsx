87943b93fc1bec70a803a78c38ff338ff58c00dc
Katya Bychkova <kats@list.ru>
2022-07-12 22:36:32 +0300

    удалила все про теги

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
-  const [tags, setTags] = useState(["tag1", "tag2", "tag3"]);
 
   const formatCount = () => {
     return count === 0 ? "empty" : count;
@@ -20,27 +19,8 @@ const Counter = () => {
     setCount((prevState) => prevState - 1);
   };
 
-  const handleTagChange = (id) => {
-    setTags((prevState) => prevState.filter((tag) => tag !== id));
-  };
-
-  const renderTags = () => {
-    if (tags.length === 0) return "No tags";
-
-    return tags.map((tag) => (
-      <li
-        key={tag}
-        className="btn btn-primary btn-sm m-2"
-        onClick={() => handleTagChange(tag)} // колбэк ф вызовется только тогда когда совершится клик
-      >
-        {tag}
-      </li>
-    ));
-  };
-
   return (
     <>
-      <ul>{renderTags()}</ul>
       <span className={getBagesClasses()}>{formatCount()}</span>
       <button className="btn btn-primary btn-sm m-2" onClick={handleIncrement}>
         +
